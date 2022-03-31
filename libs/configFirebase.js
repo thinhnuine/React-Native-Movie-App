0027457d1718491d0dc60bcea2e95d0f66a9f675
Thinh Nguyen <thinhnguyen@Ducs-MacBook-Pro.local>
2022-03-31 15:57:17 +0700

    setup firebase

---
+++ libs/configFirebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {} from "firebase/auth";
+import {} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-cx0jVbmlnfVGdf4XMz3CBgl9F1Qj7R4",
+  authDomain: "movies-app-160db.firebaseapp.com",
+  projectId: "movies-app-160db",
+  storageBucket: "movies-app-160db.appspot.com",
+  messagingSenderId: "714086040846",
+  appId: "1:714086040846:web:d9bc9ec3946d407c06c35f",
+  measurementId: "G-5P996SQP8R",
+};
+
+firebase.initializeApp(firebaseConfig);
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectAuth, projectFirestore };
