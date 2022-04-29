44491eff46c9fad75ddcf5a79b0244b3716aee71
Thinh Nguyen <thinhnguyen@Ducs-MacBook-Pro.local>
2022-04-29 22:27:22 +0700

    hot fix

--- screens/ResetPassword.js
+++ screens/ResetPassword.js
@@ -1,81 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, TextInput, StyleSheet, Text, View, Image, Pressable, Dimensions } from "react-native";
-import { Link } from "@react-navigation/native";
+import { TextInput, StyleSheet, Text, View, Pressable, Dimensions } from "react-native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function ResetPassword() {
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+
   return (
-    <View style={styles.viewContainer}>
-      <View style={styles.content}>
-        <Image style={styles.brandApp} source={require("../assets/auth/login.png")} />
-      </View>
-      <View style={styles.formContainer}>
-        <Text style={styles.textGreet}>Reset Password</Text>
-        <TextInput style={styles.formInput} placeholder="Enter username" />
-        <TextInput
-          style={styles.formInput}
-          secureTextEntry={true}
-          textContentType="password"
-          placeholder="Enter password"
-        />
-        <View style={styles.buttonContainer}>
-          <Pressable style={styles.button}>
-            <Text style={styles.textButton}>Reset</Text>
-          </Pressable>
+    <SafeAreaView style={styles.viewContainer}>
+        <View style={styles.formContainer}>
+          <TextInput
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={styles.formInput}
+            placeholder="Email"
+            placeholderTextColor={"#f1f3f5"}
+          />
+          <View style={styles.buttonContainer}>
+            <Pressable style={styles.button}>
+              <Text style={styles.textButton}>Reset Password</Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   viewContainer: {
-    backgroundColor: "#ffd43b",
+    backgroundColor: "black",
     flex: 1,
     alignItems: "center",
-    marginBottom: 0,
   },
-  content: {
+  container: {
     flex: 1,
-    paddingTop: "5%",
-  },
-  brandApp: {
-    height: "100%",
-    width: Dimensions.get("screen").width / 1.7,
-    resizeMode: "cover",
-  },
-  textGreet: {
-    fontSize: 40,
-    fontWeight: "500",
-    color: "#ffd43b",
   },
   textRegister: {
     fontStyle: "italic",
     color: "#fa5252",
   },
   textDirect: {
+    textAlign: "center",
+    color: "#fff",
     marginBottom: 30,
   },
+  errorMessage: {
+    color: "red",
+    textAlign: "center",
+  },
   formContainer: {
-    flex: 1.5,
-    backgroundColor: "#fff",
-    borderTopStartRadius: 60,
-    borderTopEndRadius: 60,
-    width: "100%",
-    paddingTop: 40,
-    paddingLeft: 30,
-    paddingRight: 30,
+    flex: 1,
+    marginTop: Dimensions.get("screen").height / 3,
+    width:"70%",
   },
   formInput: {
-    borderBottomWidth: 1,
-    padding: 10,
+    borderWidth: 1,
+    padding: 15,
     marginBottom: 20,
+    backgroundColor: "#444",
+    borderRadius: 5,
+    color: "#fff",
   },
   forgotLink: {
-    fontSize: 16,
-    color: "#ccc",
-    textAlign: "right",
-    marginTop: 5,
+    fontSize: 13,
+    color: "#fff",
+    textAlign: "center",
+    marginTop: 30,
     marginBottom: 20,
   },
   buttonContainer: {
@@ -88,10 +79,15 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderRadius: 5,
     elevation: 3,
-    backgroundColor: "#ffd43b",
+    backgroundColor: "black",
+    borderColor: "#444",
+    borderWidth: 2,
     width: "60%",
+    color: "#fff",
   },
   textButton: {
-    color: "black",
+    color: "#fff",
+    fontWeight: "500",
+    fontSize: 15,
   },
 });
